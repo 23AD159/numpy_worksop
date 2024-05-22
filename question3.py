c7345eae4be4baf1bc3bf2c7c4875bc2ae59ec78
23AD159 <153845833+23AD159@users.noreply.github.com>
2024-05-22 10:43:26 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,15 @@
 #write a program to find the factorial of a nummber
+
+num = int(input("Enter a number: "))
+
+fact = 1
+
+if num < 0:
+   print("invalid number")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       fact = fact*i
+   print("The factorial of",num,"is",fact)
+
