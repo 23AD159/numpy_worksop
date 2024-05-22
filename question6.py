76d4bfef31e1a3f76c3bb11de7440e34c31ef7d8
23AD159 <153845833+23AD159@users.noreply.github.com>
2024-05-22 11:13:12 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,17 @@
 #write a program to find the maximum of two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+
+if(num1 > num2):
+  print(num1, "is greater")
+elif(num1 < num2):
+    print(num2, "is greater")
+else:
+    print("Both are equal")
+
+
+
+
+
+
